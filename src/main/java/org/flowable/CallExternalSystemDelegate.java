6a366d868e393f2bbcf651760024e79f83b48875
zhuangjx <zhuangjx@wangsu.com>
2020-10-29 13:41:16 +0800

    1.分支push合并

--- src/main/java/org/flowable/CallExternalSystemDelegate.java
+++ src/main/java/org/flowable/CallExternalSystemDelegate.java
@@ -8,6 +8,7 @@ public class CallExternalSystemDelegate implements JavaDelegate{
 	public void execute(DelegateExecution execution) {
 		// TODO Auto-generated method stub
 		System.out.println(System.getProperty("os"));
+		System.out.println(System.getenv());
 		System.out.println("Calling the external system for employee "
 	            + execution.getVariable("employee"));
 	}
